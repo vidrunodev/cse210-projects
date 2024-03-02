27b94ce38a1addfc565c8ae12bf1b67f6d5f097b
vidrunodev <brunoch20@hotmail.com>
2024-03-02 09:52:09 -0500

    Prep2 plus stretch challenge

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,66 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("");
+        Console.Write("Enter your score: ");
+        string valueFromUser = Console.ReadLine();
+
+        int x = int.Parse(valueFromUser);
+        string letter = "";
+        string sign = "";
+
+        if (x >= 90)
+        {
+            letter = "A";
+        }
+        else if (x >= 80)
+        {
+            letter = "B";
+        }
+        else if (x >= 70)
+        {
+            letter = "C";
+        }
+        else if (x >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        int lastDigit = x % 10;
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+
+        if (letter == "A" && sign == "+")
+        {
+            letter = "A";
+            sign = "-";
+        }
+        else if (letter == "F" && (sign == "+" || sign == "-"))
+        {
+            sign = "";
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine($"Your score is {letter}{sign}");
+
+        if (letter == "A" || letter == "B" || letter == "C")
+        {
+            Console.WriteLine("Congratulations, you passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Every setback is a setup for a comeback; use this experience as fuel to ignite your determination and excel in the next opportunity.");
+        }
+        Console.WriteLine("");
     }
 }
