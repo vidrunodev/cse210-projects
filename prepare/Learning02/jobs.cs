437e6a71746d3cbacd7b03ceae43790f929c63f7
vidrunodev <brunoch20@hotmail.com>
2024-03-08 00:10:39 -0500

    created job.cs file and job class

---
+++ prepare/Learning02/jobs.cs
@@ -0,0 +1,10 @@
+public class Job
+{
+    public string _jobTitle;
+    public string _company;
+    public string _startDate;
+    public string _endDate;
+
+    // To add display method later here
+}
+
