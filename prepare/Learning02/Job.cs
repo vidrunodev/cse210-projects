719cd05bd743b79d3c86175cd53900e4972a3b27
vidrunodev <brunoch20@hotmail.com>
2024-03-08 00:18:22 -0500

    Jobs.cs file , class and after test for job class

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -4,7 +4,6 @@ public class Job
     public string _company;
     public string _startDate;
     public string _endDate;
-
-    // To add display method later here
+    
+    //Display method here later
 }
-
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startDate = "2019";
+        job1._endDate = "2022";
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startDate = "2022";
+        job2._endDate = "2023";
+
+        // Display company using dot notation
+        Console.WriteLine(job1._company);
+        Console.WriteLine(job2._company);
+
     }
 }
