b4269e253ad29371d3a00df884bd9b9e6b3058dd
vidrunodev <brunoch20@hotmail.com>
2024-03-29 00:15:32 -0500

    Polymorphism Learning Activity

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Shape
+{
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public virtual double GetArea()
+    {
+        return 0;
+    }
+}
